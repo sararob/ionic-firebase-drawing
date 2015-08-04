bb0c5449d6b9f775e719e3f34cf0f059ded75aea
mhartington <mikehartington@gmail.com>
2015-08-03 21:33:03 -0400

    fix touch events, rough auth setup

--- www/js/app.js
+++ www/js/app.js
@@ -100,40 +100,44 @@ angular.module('starter', ['ionic', 'firebase'])
   }
 
   //Keep track of if the mouse is up or down
-  myCanvas.onmousedown = function() {
+  $(myCanvas).on('mousedown touchstart', function() {
     mouseDown = 1;
-  };
-  myCanvas.onmouseup = function() {
+  })
+  myCanvas.onmousedown = function() {};
+
+  $(myCanvas).on('mouseup touchend', function() {
     mouseDown = 0;
     lastPoint = null;
-  };
+  })
+  myCanvas.onmouseup = function() {};
 
   //Set the active color
   $('.colorbox[style="background-color: rgb(0, 0, 0);"]').addClass("active");
   //Draw a line from the mouse's last position to its current position
 
 
+  var offset = $('canvas').offset();
+  var x1, y1, x0, y0, dx, dy, sx,sy, err, er;
   //Draw a line from the mouse's last position to its current position
   var drawLineOnMouseMove = function(e) {
     if (!mouseDown) return;
     e.preventDefault();
 
     // Bresenham's line algorithm. We use this to ensure smooth lines are drawn
-    var offset = $('canvas').offset();
-    var x1 = Math.floor((e.pageX - offset.left) / pixSize - 1);
-    var y1 = Math.floor((e.pageY - offset.top) / pixSize - 1);
-    var x0 = (lastPoint == null) ? x1 : lastPoint[0];
-    var y0 = (lastPoint == null) ? y1 : lastPoint[1];
-    var dx = Math.abs(x1 - x0);
-    var dy = Math.abs(y1 - y0);
-    var sx = (x0 < x1) ? 1 : -1;
-    var sy = (y0 < y1) ? 1 : -1;
-    var err = dx - dy;
+     x1 = Math.floor((e.pageX - offset.left) / pixSize - 1);
+     y1 = Math.floor((e.pageY - offset.top) / pixSize - 1);
+     x0 = (lastPoint == null) ? x1 : lastPoint[0];
+     y0 = (lastPoint == null) ? y1 : lastPoint[1];
+     dx = Math.abs(x1 - x0);
+     dy = Math.abs(y1 - y0);
+     sx = (x0 < x1) ? 1 : -1;
+     sy = (y0 < y1) ? 1 : -1;
+     err = dx - dy;
     while (true) {
       pixelDataRef.child(x0 + ":" + y0).set(currentColor === "fff" ? null : currentColor);
 
       if (x0 == x1 && y0 == y1) return;
-      var e2 = 2 * err;
+       e2 = 2 * err;
       if (e2 > -dy) {
         err = err - dy;
         x0 = x0 + sx;
@@ -147,10 +151,42 @@ angular.module('starter', ['ionic', 'firebase'])
   };
 
 
-  // Drawing is broken on mobile ATM (◞‸◟；)
-  // TODO: Figure out whats up.
+
+  var drawLineOnTouchMove = function(e) {
+    if (!mouseDown) {
+      return;
+    }
+    e.preventDefault();
+     x1 = Math.floor((e.originalEvent.touches[0].pageX - offset.left) / pixSize - 1);
+     y1 = Math.floor((e.originalEvent.touches[0].pageY - offset.top) / pixSize - 1);
+     x0 = (lastPoint === null) ? x1 : lastPoint[0];
+     y0 = (lastPoint === null) ? y1 : lastPoint[1];
+     dx = Math.abs(x1 - x0);
+     dy = Math.abs(y1 - y0);
+     sx = (x0 < x1) ? 1 : -1;
+     sy = (y0 < y1) ? 1 : -1;
+     err = dx - dy;
+    pixelDataRef.child(x0 + ':' + y0).set(currentColor === 'fff' ? null : currentColor);
+    if (x0 === x1 && y0 === y1) return;
+     e2 = 2 * err;
+    if (e2 > -dy) {
+      console.log("here");
+      err = err - dy;
+      x0 = x0 + sx;
+    }
+    if (e2 < dx) {
+      err = err + dx;
+      y0 = y0 + sy;
+    }
+
+    lastPoint = [x1, y1];
+  };
+
   $(myCanvas).on('mousedown', drawLineOnMouseMove);
   $(myCanvas).on('mousemove', drawLineOnMouseMove);
+
+  $(myCanvas).on('touchstart', drawLineOnTouchMove);
+  $(myCanvas).on('touchmove', drawLineOnTouchMove);
 
   // Add callbacks that are fired any time the pixel data changes and adjusts the canvas appropriately.
   // Note that child_added events will be fired for initial pixel data as well.
